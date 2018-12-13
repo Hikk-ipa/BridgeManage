010ac0fc5c154e669d81f40267abbb9db42fc46b
Hikk <huihui8905@qq.com>
2018-12-13 20:23:31 +0800

    初始化

---
+++ BridgeManage/BridgeManage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BridgeManage
+//
+//  Created by 朱晨辉 on 2018/12/13.
+//  Copyright © 2018年 hikk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
